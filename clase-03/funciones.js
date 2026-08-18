1a8d457503194e5f8849fe54f0409f1e2b7ce229
Tyncho <tyncho.zabala@gmail.com>
2026-08-17 22:19:35 -0300

    ejercicio funciones

---
+++ clase-03/funciones.js
@@ -0,0 +1,15 @@
+function suma (a,b) {
+    return a+b;
+}
+const resta = function (a,b) {
+    return a-b;
+}
+const multiplicacion = (a,b) => {
+    return a*b;
+}
+console.log("La suma es:");
+console.log (suma (1,2));
+console.log("La resta es:");
+console.log(resta(1,2));
+console.log("La multiplicación es:");
+console.log(multiplicacion(1,2));
